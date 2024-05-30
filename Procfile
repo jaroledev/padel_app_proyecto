e13be4948bf2c3d4ddc68e255eca61abb5011097
jaroledev <dawjarole@gmail.com>
2024-05-30 23:13:58 +0200

    Primer commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn padel_app_proyecto.wsgi 
---
+++ padel_app_proyecto/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for padel_app_proyecto project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'padel_app_proyecto.settings')
+
+application = get_wsgi_application()
