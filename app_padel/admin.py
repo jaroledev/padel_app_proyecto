24210646532d155648faf9c3f7f43862c65b43f6
jaroledev <dawjarole@gmail.com>
2024-06-08 01:53:50 +0200

    Cambio en la vista Admin para poder ver la horas disponibles

--- app_padel/admin.py
+++ app_padel/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Reserva, Club, Pista, DetallesClub, Dimensiones
+
+class DimensionesAdmin(admin.ModelAdmin):
+    list_display = ('horas_disponibles')
 # Register your models here.
 
 admin.site.register(Reserva)
 admin.site.register(Club)
 admin.site.register(Pista)
 admin.site.register(DetallesClub)
-admin.site.register(Dimensiones)
+admin.site.register(Dimensiones, DimensionesAdmin)
